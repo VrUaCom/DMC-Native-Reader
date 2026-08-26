9f04b8862a6037007c8322d9af98ddd5d7bc6351
VrUaCom <rusak.viktor.vr@gmail.com>
2026-08-26 19:55:24 +0200

    build: bump Android reader to v5 system handler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.dmcrengine.nativeviewer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.4.0-samsung-routing"
+        versionCode = 5
+        versionName = "0.5.0-system-handler"
 
         externalNativeBuild {
             cmake {
