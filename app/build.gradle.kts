c6d16610cc67aeab631997224341978532bf0fcf
VrUaCom <rusak.viktor.vr@gmail.com>
2026-09-06 17:44:33 +0200

    Version UI2 debug build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.dmcrengine.nativereader"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.0.1-debug-ui1"
+        versionCode = 12
+        versionName = "1.0.2-debug-ui2"
 
         externalNativeBuild {
             cmake {
