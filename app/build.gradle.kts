b532f07c7feb465bc0c966179a8804d367364a1d
VrUaCom <rusak.viktor.vr@gmail.com>
2026-09-07 07:50:52 +0200

    build: bump child-resource gallery device build to code17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.dmcrengine.nativereader"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.1.4-debug-dds-ptx"
+        versionCode = 17
+        versionName = "1.1.5-debug-child-gallery"
 
         externalNativeBuild {
             cmake {
