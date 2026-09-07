a0bc88946020e65efcb50ca54f5b63bc0581c8ed
VrUaCom <rusak.viktor.vr@gmail.com>
2026-09-07 07:18:23 +0200

    v1: bump DDS PTX Samsung acceptance build to code16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.dmcrengine.nativereader"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.1.3-debug-v1-hardening"
+        versionCode = 16
+        versionName = "1.1.4-debug-dds-ptx"
 
         externalNativeBuild {
             cmake {
