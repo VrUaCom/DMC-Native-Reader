0ef873d7f459b0426500a19329bf52b7c0437d49
VrUaCom <rusak.viktor.vr@gmail.com>
2026-09-09 16:03:34 +0200

    Android: bump textured companion device build to versionCode 23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         applicationId = "com.dmcrengine.nativereader"
         minSdk = 26
         targetSdk = 36
-        versionCode = 22
+        versionCode = 23
         versionName = "1.0"
 
         externalNativeBuild {
