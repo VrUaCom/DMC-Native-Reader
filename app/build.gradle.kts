95ba9a859fe5097d8c9a37fb30b304598cc47fa9
VrUaCom <rusak.viktor.vr@gmail.com>
2026-09-09 13:47:13 +0200

    Android: bump Native Reader 1.0 device build to versionCode 22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         applicationId = "com.dmcrengine.nativereader"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
+        versionCode = 22
         versionName = "1.0"
 
         externalNativeBuild {
