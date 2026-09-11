67d3eb6dfe513bb2b0a46a970a064e4923175610
VrUaCom <rusak.viktor.vr@gmail.com>
2026-09-11 05:55:47 +0200

    Bump Android device-test build to v27

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         applicationId = "com.dmcrengine.nativereader"
         minSdk = 26
         targetSdk = 36
-        versionCode = 26
+        versionCode = 27
         versionName = "1.0"
 
         externalNativeBuild {
