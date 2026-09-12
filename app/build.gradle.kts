f71fc24d104c8c96f98e58d49fabad847bcaa5e3
VrUaCom <rusak.viktor.vr@gmail.com>
2026-09-12 15:01:41 +0200

    build: bump TM2 device-test APK to v30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "com.dmcrengine.nativereader"
         minSdk = 26
         targetSdk = 36
-        versionCode = 29
-        versionName = "1.0.2"
+        versionCode = 30
+        versionName = "1.0.3"
 
         externalNativeBuild {
             cmake {
