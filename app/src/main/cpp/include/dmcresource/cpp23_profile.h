baf1c46997c38ac8d4b2133d2d1823f5f9606e49
VrUaCom <rusak.viktor.vr@gmail.com>
2026-09-15 17:45:07 +0200

    core: require Native Reader C++23 utility baseline

--- app/src/main/cpp/include/dmcresource/cpp23_profile.h
+++ app/src/main/cpp/include/dmcresource/cpp23_profile.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <bit>
 #include <expected>
 #include <string_view>
+#include <utility>
 #include <version>
 
 #if __cplusplus < 202302L
@@ -12,11 +14,21 @@
 #error "DMC Native Reader C++23 profile requires std::expected >= 202202L"
 #endif
 
+#if !defined(__cpp_lib_byteswap) || __cpp_lib_byteswap < 202110L
+#error "DMC Native Reader C++23 profile requires std::byteswap"
+#endif
+
+#if !defined(__cpp_lib_to_underlying) || __cpp_lib_to_underlying < 202102L
+#error "DMC Native Reader C++23 profile requires std::to_underlying"
+#endif
+
 namespace dmcresource::cpp23 {
 
 inline constexpr std::string_view kProfile = "dmc.native-reader.cpp23";
 inline constexpr long kLanguageLevel = __cplusplus;
 inline constexpr long kExpectedFeature = __cpp_lib_expected;
+inline constexpr long kByteswapFeature = __cpp_lib_byteswap;
+inline constexpr long kToUnderlyingFeature = __cpp_lib_to_underlying;
 
 template <class T, class E>
 using Result = std::expected<T, E>;
